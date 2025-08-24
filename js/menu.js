bea73a69cb5d45ce8c6eeb9a26418752887d1dc9
daniel22s <cruzdani543@gmail.com>
2025-08-24 01:47:18 -0500

    Agregar links de whtsapp

--- js/menu.js
+++ js/menu.js
@@ -1,17 +1,23 @@
-(function(){
-    const openButton = document.querySelector('.nav_menu');
-    const menu = document.querySelector('.nav_link');
+(function() {
+    const openButton = document.querySelector('.nav_menu');          
+    const menu = document.querySelector('.nav_link--menu');           
     const closeMenu = document.querySelector('.nav_close');
 
-    openButton.addEventListener('click', ()=>{
-        menu.classList.add('nav_link--show');
+    // Abrir menú
+    openButton.addEventListener('click', () => {
+        menu.classList.add('nav_link--show');  
     });
 
-    closeMenu.addEventListener('click', ()=>{
+    // Cerrar menú con el botón close
+    closeMenu.addEventListener('click', () => {
         menu.classList.remove('nav_link--show');
     });
 
-    
-
-
+    // Cerrar menú al hacer click en cualquiera de los enlaces
+    const links = document.querySelectorAll('.nav_link--menu .nav_links');
+    links.forEach(link => {
+        link.addEventListener('click', () => {
+            menu.classList.remove('nav_link--show');
+        });
+    });
 })();
